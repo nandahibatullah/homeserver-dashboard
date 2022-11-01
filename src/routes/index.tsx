04774c8511b53c432285c48980ac9e869351d293
Nanda Hibatullah <nandahibatullah@gmail.com>
2022-11-02 01:05:44 +1300

    feat(basic-login): use auth provider across app to authenticate routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,57 @@
-import React from 'react';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import React, { ReactNode } from 'react';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+} from 'react-router-dom';
+import { AuthProvider, useAuth } from '../providers/AuthProvider';
 import { Dashboard } from '../pages/Dashboard';
+import { Login } from '../pages/Login';
 import { VitePage } from '../pages/VitePage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <VitePage />,
-  },
-  {
-    path: 'dashboard',
-    element: <Dashboard />,
-  },
-]);
+const AuthenticatedRoute = (props: { children: ReactNode }): JSX.Element => {
+  const { loggedIn } = useAuth();
+  console.log(loggedIn);
+
+  if (!loggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{props.children}</>;
+};
+
+const AuthProviderLayout = () => (
+  <AuthProvider>
+    <Outlet />
+  </AuthProvider>
+);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<AuthProviderLayout />}>
+      <Route
+        path="/"
+        element={
+          <AuthenticatedRoute>
+            <VitePage />
+          </AuthenticatedRoute>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <AuthenticatedRoute>
+            <Dashboard />
+          </AuthenticatedRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
+);
 
 export const Routes = () => {
   return <RouterProvider router={router} />;
